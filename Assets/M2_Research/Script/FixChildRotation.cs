94e862fb913b19fb40ab06664ae1c93585fed6bf
ShimiShun <b1013077@gmail.com>
2018-07-01 14:50:23 +0900

    スクリプト追加

---
+++ Assets/M2_Research/Script/FixChildRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixChildRotation : MonoBehaviour {
+
+
+	private Quaternion _parent;
+
+
+	void Start(){
+		_parent = this.transform.transform.rotation;
+	}
+
+
+
+	void Update ()
+	{
+		gameObject.transform.rotation = Quaternion.Euler(_parent.x, _parent.y, _parent.z);
+	}
+}
